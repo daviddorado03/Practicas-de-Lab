9b1cc0d944be01cde56efed3e617c20b660fbb11
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-08-26 08:23:40 -0500

    practica03 08:23

---
+++ practica03/Personaje.java
@@ -0,0 +1,11 @@
+public class Personaje {
+    private String nombre;
+    private int edad;
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+}

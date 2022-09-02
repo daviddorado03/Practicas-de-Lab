5d204b57322be11912435ff2f9189f9d3a5c5c1d
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-09-02 07:55:30 -0500

    practica04 07:55

--- practica04/PruebaPersonaje.java
+++ practica04/PruebaPersonaje.java
@@ -1,10 +1,16 @@
 public class PruebaPersonaje {
     public static void main(String[] args){
-        Personaje alumno = new Personaje();
-        alumno.setNombre("Kobie Bryant");
+        Personaje alumno = new Personaje("Kal El");
         alumno.saludar();
+        System.out.println("Nombre: " + alumno.getNombre() + "Edad: " + alumno.getEdad());
         System.out.println("Modificando el nombre " + alumno.getNombre());
         alumno.setNombre("Michael Jordan");
         alumno.saludar();
+        if (!alumno.setEdad(30)){
+            System.out.println("Edad sin cambios");
+        }
+        System.out.println("Nombre: " + alumno.getNombre() + "Edad: " + alumno.getEdad());
+        System.out.println("Modificando edad en 130");
+        
     }
 }

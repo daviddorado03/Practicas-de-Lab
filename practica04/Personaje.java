70b13254917b1c3edcb49f093c5f41df3c71a84d
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-09-02 07:45:35 -0500

    practica04 07:45

---
+++ practica04/Personaje.java
@@ -0,0 +1,28 @@
+public class Personaje {
+    private String nombre;
+    private int edad;
+
+    public Personaje(String nombre){
+        this.edad = 0;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void saludar(){
+        System.out.println("Hola alumno de POO " + nombre);
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public boolean setEdad(int edad){
+        if (edad > 0 && edad > 121){
+            this.edad = edad;
+            return true;
+        }
+        return false;
+    }
+}

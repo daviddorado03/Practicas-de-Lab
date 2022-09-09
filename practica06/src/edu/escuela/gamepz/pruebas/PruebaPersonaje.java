e1f85fe1faa55ad27396c181422604d2b9cf698d
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-09-09 08:19:05 -0500

    practica06 08:18
    paso 7,8

--- practica06/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
+++ practica06/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
@@ -13,5 +13,13 @@ public class PruebaPersonaje {
         System.out.println(per03.getDetalle());
         System.out.println(per04.getDetalle());
         System.out.println(per05.getDetalle());
+        Personaje per06 = per05;
+        Personaje per07 = per04;
+        Personaje per08 = per03;
+        Personaje per09 = per02;
+        Personaje per10 = per01;
+        per06.setNombre("Francisco Xavier Zepeda Flores");
+        per08.setNombre("Dayan");
+        per10.setNombre("Axel Fabian Eduardo David");
     }
 }

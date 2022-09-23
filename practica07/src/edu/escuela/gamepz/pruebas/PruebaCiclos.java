86ee7580e0926db7683c7d48cb7baa898e2c9409
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-09-23 07:52:09 -0500

    practica07 07:52

--- practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -2,10 +2,21 @@ package edu.escuela.gamepz.pruebas;
 import edu.escuela.gamepz.personajes.Personaje;
 
 public class PruebaCiclos {
-    Personaje per01 = new Personaje("Diego");
-    Personaje per02 = new Personaje("Almendra");
-    Personaje per03= new Personaje("Bianca");
-    Personaje per04 = new Personaje("Jordan");
-    int n = 0;
-    int m = 0;
+    public static void main(String[] args){
+        Personaje per01 = new Personaje("Diego");
+        Personaje per02 = new Personaje("Almendra");
+        Personaje per03= new Personaje("Bianca");
+        Personaje per04 = new Personaje("Jordan");
+        int n = 0;
+        int m = 0;
+        while (n>=m){
+            n = ((int)(Math.random()*100));
+            m = ((int)(Math.random()*100));
+        }
+        System.out.println("n = " + n);
+        System.out.println("m = " + m);
+        for (int i = n; i < m; i++){
+            
+        }
+    }
 }

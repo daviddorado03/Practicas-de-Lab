4990772c81a83bceacd9099aa7ca12a3026fa46f
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-09-23 07:41:59 -0500

    practica07 07:41

---
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -0,0 +1,11 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.Personaje;
+
+public class PruebaCiclos {
+    Personaje per01 = new Personaje("Diego");
+    Personaje per02 = new Personaje("Almendra");
+    Personaje per03= new Personaje("Bianca");
+    Personaje per04 = new Personaje("Jordan");
+    int n = 0;
+    int m = 0;
+}

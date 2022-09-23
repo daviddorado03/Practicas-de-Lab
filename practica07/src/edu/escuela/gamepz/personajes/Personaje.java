253833d6da89f959ac88ea47cb838cd7a34a21f2
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-09-23 07:33:42 -0500

    practica07 07:33

---
+++ practica07/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,34 @@
+package edu.escuela.gamepz.personajes;
+
+public class Personaje {
+    private String nombre;
+    private int edad;
+
+    public Personaje(String nombre){
+        this.nombre = nombre;
+    }
+    public void setNombre(String nombre){
+        if ((nombre.length() > 5) && (nombre.length() < 25)){
+            this.nombre = nombre;
+        }
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void saludar(){
+        System.out.println("Hola alumno de POO " + nombre);
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public boolean setEdad(int edad){
+        if (edad > 0 && edad < 121){
+            this.edad = edad;
+            return true;
+        }
+        return false;
+    }
+    public String getDetalle(){
+        return nombre + "\t" + edad;
+    }
+}

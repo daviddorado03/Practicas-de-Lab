431c65bf8fc5bfac966b827f1dc5266f015bbfdf
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-11-04 08:47:30 -0600

    practica12 08:47

---
+++ practica12/src/edu/escuela/gamepz/utils/PersException.java
@@ -0,0 +1,12 @@
+package edu.escuela.gamepz.utils;
+
+public class PersException extends Exception{
+    private int pos;
+    public PersException(String msg, int pos){
+        super(msg);
+        this.pos = pos;
+    }
+    public int getPos() {
+        return pos;
+    }
+}
---
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -0,0 +1,8 @@
+package edu.escuela.gamepz.utils;
+import edu.escuela.gamepz.personajes.Personaje;
+
+public class Tablero {
+    private static final int MAX_SIZE = 10;
+    private static Personaje [] personajes;
+    private Tablero(){};
+}

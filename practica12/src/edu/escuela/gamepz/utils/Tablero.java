17b395dd3fe12ac35a6ae36bfd523ac0859a96d4
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-11-04 09:00:39 -0600

    practica12 09:00

--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -3,6 +3,16 @@ import edu.escuela.gamepz.personajes.Personaje;
 
 public class Tablero {
     private static final int MAX_SIZE = 10;
-    private static Personaje [] personajes;
+    private static Personaje[] personajes = new Personaje[MAX_SIZE];
     private Tablero(){};
+    public static void mostrar(){
+        System.out.println("\nInicio *** Contenido del arreglo en Tablero");
+        for (Personaje pos : personajes) {
+            if ( pos == null){
+                System.out.println(pos + "---");
+            } else {
+                System.out.println(pos + " " + Personaje);
+            } System.out.println();
+        }
+    }
 }

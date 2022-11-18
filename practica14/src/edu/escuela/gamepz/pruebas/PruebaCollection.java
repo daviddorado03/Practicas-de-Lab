b7098a5f177b61d3dd658a104140469143549aff
Jose David Dorado Treviño <38193853@uaz.edu.mx>
2022-11-18 12:11:10 -0600

    practica14 12:11

--- practica14/src/edu/escuela/gamepz/pruebas/PruebaCollection.java
+++ practica14/src/edu/escuela/gamepz/pruebas/PruebaCollection.java
@@ -1,13 +1,9 @@
 package edu.escuela.gamepz.pruebas;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Scanner;
